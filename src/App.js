75129f5ccd27529fe584151bb5e47adda38e9144
Joseph Tong <joseph@tradewing.com>
2020-03-25 11:52:28 -0700

    interview set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={'https://tradewing-dev.herokuapp.com/cdn/storage/profileFiles/tGDWWPSsx8u8uGCFo/original/tGDWWPSsx8u8uGCFo.jpeg'} alt='wonky'/>
       </header>
     </div>
   );
