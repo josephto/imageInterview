a8595b893ab7424937e58f8febd818ca76de7ceb
Joseph Tong <joseph@tradewing.com>
2020-03-25 11:57:45 -0700

    image setup and readme update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={'https://tradewing-dev.herokuapp.com/cdn/storage/profileFiles/tGDWWPSsx8u8uGCFo/original/tGDWWPSsx8u8uGCFo.jpeg'} alt='wonky'/>
+        <img src={'/wonky_image.jpeg'} alt='wonky'/>
       </header>
     </div>
   );
